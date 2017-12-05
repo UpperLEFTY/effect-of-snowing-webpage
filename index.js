6f49ab2c0687dac127a6b67560da6923b8c6f2bd
Essence Alexander <upperlefty@zoho.eu>
2017-12-05 19:38:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+</script><script>
+// Set the number of snowflakes (more than 30 - 40 not recommended)
+var snowmax=35
+
+// Set the colors for the snow. Add as many colors as you like
+var snowcolor=new Array("#aaaacc","#ddddFF","#ccccDD")
+
+// Set the fonts, that create the snowflakes. Add as many fonts as you like
+var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
+
+// Set the letter that creates your snowflake (recommended:*)
+var snowletter="*"
+
