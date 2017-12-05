0228b4f9f8b6727eb24d288718e14c5255820ad7
Essence Alexander <upperlefty@zoho.eu>
2017-12-05 19:39:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
 // Set the letter that creates your snowflake (recommended:*)
 var snowletter="*"
 
+// Set the speed of sinking (recommended values range from 0.3 to 2)
+var sinkspeed=0.6
+
+// Set the maximal-size of your snowflaxes
+var snowmaxsize=22
+
+// Set the minimal-size of your snowflaxes
+var snowminsize=8
+
+// Set the snowing-zone
+// Set 1 for all-over-snowing, set 2 for left-side-snowing 
+// Set 3 for center-snowing, set 4 for right-side-snowing
+var snowingzone=3
+
+///////////////////////////////////////////////////////////////////////////
+// CONFIGURATION ENDS HERE
+///////////////////////////////////////////////////////////////////////////
+
+
